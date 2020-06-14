9b74c7a5dd83e1e8d358cbb53d62e2f15058967a
Anewil <bartekwwb@gmail.com>
2020-06-14 16:28:47 +0200

    Added bash scripts

---
+++ build.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/Anewil/sword_localization .
+pip install -r requirments.txt
--- predict.py
+++ predict.py
@@ -25,53 +25,52 @@ writer = None
 (W, H) = (None, None)
 index = 1
 
-with open(os.path.sep.join([script_dir, "result.txt"]), 'w') as result:
-    while True:
-        # read the next frame from the file
-        (grabbed, frame) = vs.read()
-        # if the frame was not grabbed, then we have reached the end
-        # of the stream
-        if not grabbed:
-            break
-        # if the frame dimensions are empty, grab them
-        if W is None or H is None:
-            (H, W) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-                                     swapRB=True, crop=False)
-        net.setInput(blob)
-        layerOutputs = net.forward(ln)
-        boxes = []
-        confidences = []
-        frame = {}
-        # loop over each of the layer outputs
-        for output in layerOutputs:
-            # loop over each of the detections
-            for detection in output:
-                # extract the class ID and confidence (i.e., probability)
-                # of the current object detection
-                scores = detection[5:]
-                classID = np.argmax(scores)
-                confidence = scores[classID]
-                # filter out weak predictions by ensuring the detected
-                # probability is greater than the minimum probability
-                if confidence > 0.1:
-                    box = detection[0:4] * np.array([W, H, W, H])
-                    (centerX, centerY, width, height) = box.astype("int")
-                    if classID not in frame:
-                        frame[classID] = (centerX, centerY)
-                    if len(frame.keys()) > 1:
-                        break
+while True:
+    # read the next frame from the file
+    (grabbed, frame) = vs.read()
+    # if the frame was not grabbed, then we have reached the end
+    # of the stream
+    if not grabbed:
+        break
+    # if the frame dimensions are empty, grab them
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    layerOutputs = net.forward(ln)
+    boxes = []
+    confidences = []
+    frame = {}
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability)
+            # of the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > 0.1:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                if classID not in frame:
+                    frame[classID] = (centerX, centerY)
+                if len(frame.keys()) > 1:
+                    break
 
-        line = ''
-        for i in range(2):
-            if i in frame:
-                line += str(frame[i][0]) + ',' + str(frame[i][1])
-            else:
-                line += '#,#'
-            if i == 0:
-                line += ','
-        line += '\n'
-        result.write(line)
-        index += 1
+    line = ''
+    for i in range(2):
+        if i in frame:
+            line += str(frame[i][0]) + ',' + str(frame[i][1])
+        else:
+            line += '#,#'
+        if i == 0:
+            line += ','
+    line += '\n'
+    print(line)
+    index += 1
 
 vs.release()
